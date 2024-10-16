5527a1d6070bedbd647ccf60ac7f10f2258d62a0
koushikhazra <hkoushik058@gmail.com>
2024-10-16 19:36:16 +0530

    first commit

---
+++ javascript/first.js
@@ -0,0 +1,11 @@
+// in c programing  we use printf for printing 
+// in c++ we use cout for printing
+// in javascript we  use console.log() for printing
+//EX:
+console.log("hello world");
+console.log("my name is koushik hazra");
+a=5;
+b=7;
+console.log(a+b);
+
+
